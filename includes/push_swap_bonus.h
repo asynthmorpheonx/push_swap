a435f9c530c4b5274a8146344ded7a2886d06453
Mohamed El mouhib <mel-mouh@e1r9p10.1337.ma>
2025-02-22 01:28:27 +0100

    added

--- includes/push_swap_bonus.h
+++ includes/push_swap_bonus.h
@@ -6,7 +6,7 @@
 /*   By:  mel-mouh < mel-mouh@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 18:58:06 by mel-mouh          #+#    #+#             */
-/*   Updated: 2025/02/22 01:24:19 by  mel-mouh        ###   ########.fr       */
+/*   Updated: 2025/02/22 01:28:18 by  mel-mouh        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ void	ft_free(char **s1, void *s2);
 long	*c_to_d(char **c_arg, int *index);
 long	*turn_it_into_int(char *arg, int *index);
 void	check_for_doubles(long *numbers, int end);
-int		check_if_sorted_already(long *array, int end);
 void	check_if_valid(char **c_arg);
 char	*join_all(int ac, char **av);
 int		sub_check(char *c_arg);
@@ -33,15 +32,15 @@ void	ft_perror(void);
 t_list	*stor_in_list(long *nbrs, int end);
 void	swap_a(t_list **stack_a);
 void	swap_b(t_list **stack_b);
+void	swap_both(t_list **stack_a, t_list **stack_b);
 void	push_a(t_list **stack_a, t_list **stack_b);
 void	push_b(t_list **stack_a, t_list **stack_b);
 void	rotate_a(t_list **stack_a);
 void	rotate_b(t_list **stack_b);
-void	swap_both(t_list **stack_a, t_list **stack_b);
+void	rotate_both(t_list **stack_a, t_list **stack_b);
 void	reverse_rotate_a(t_list **stack_a);
 void	reverse_rotate_b(t_list **stack_b);
 void	reverse_rotate_both(t_list **stack_a, t_list **stack_b);
-void	rotate_both(t_list **stack_a, t_list **stack_b);
 int		check_if_instruction(char	*str);
 void	user_sort_execution(char *instruction, t_list **a, t_list **b);
 void	read_nd_do(t_list **a, t_list **b);
